c08a1b168abbe51aa582983761dd48df2b20656f
moi061805 <84187816+moi061805@users.noreply.github.com>
2021-06-04 14:15:58 -0400

    Update JOP.java

--- src/cont/JOP.java
+++ src/cont/JOP.java
@@ -3,6 +3,7 @@ package cont;
 import javax.swing.JOptionPane;
 
 public class JOP {
+  //This class enables me to use the JOptionPane features
   public static void msg(String msg) {
     JOptionPane.showMessageDialog(null, msg);
   }
